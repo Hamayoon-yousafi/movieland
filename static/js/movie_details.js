37a12050396d9a3e6bea940e5e730ea3747ca78e
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-10-05 16:19:58 +0430

    designed trailer for movie

---
+++ static/js/movie_details.js
@@ -0,0 +1,39 @@
+let trailer = document.querySelector('.trailer');
+let play = document.querySelector('.video-play');
+let pause = document.querySelector('.video-pause');
+
+
+function playVideo() {
+    play.classList.remove('show');
+    play.classList.add('hide');
+    
+    pause.classList.remove('hide');
+    pause.classList.add('show');
+    setTimeout(() => {
+        pause.classList.remove('show');
+        pause.classList.add('hide');
+    }, 1000);
+    trailer.play();
+}
+
+function pauseVideo() {
+    pause.classList.remove('show');
+    pause.classList.add('hide');
+    
+    play.classList.remove('hide');
+    play.classList.add('show');
+
+    trailer.pause();
+}
+
+
+trailer.addEventListener('mousemove', function(){
+    if(!trailer.paused && !pause.classList.contains('show')){
+        pause.classList.remove('hide');
+        pause.classList.add('show');
+        setTimeout(() => {
+            pause.classList.remove('show');
+            pause.classList.add('hide');
+        }, 1000);
+    }
+})

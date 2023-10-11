78e5958a2ce3154e2e7ff0fcb3dcdd33afaa707d
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-10-11 11:24:19 +0430

    designed registration form

--- user/forms.py
+++ user/forms.py
@@ -3,17 +3,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
+CSS_CLASS = 'form-control bg-dark text-white customize-input p-0 ps-2 mb-2'
+LABELS = {'username': 'Username', 'password1': 'Password', 'password2': 'Confirm Password', 'email': 'Email'}
+
 class UserRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, label in LABELS.items():
+            self.fields[field_name].label = label
+            self.fields[field_name].widget.attrs.update({'class': CSS_CLASS, 'placeholder': label})
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email')
 
 
 class CustomLoginForm(AuthenticationForm):
-    def __init__(self, request: Any = ..., *args: Any, **kwargs):
-        super().__init__(request, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         for label, field in self.fields.items():
-            field.widget.attrs.update({'class': 'form-control bg-dark text-white customize-input p-0 ps-2 mb-2', 'placeholder': 'Enter ' + label.title()})
+            field.widget.attrs.update({'class': CSS_CLASS, 'placeholder': 'Enter ' + label.title()})
 
     

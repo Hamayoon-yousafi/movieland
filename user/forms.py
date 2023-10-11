2037da425d9c2628997c6597f2b7ff6226339b82
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2023-10-11 16:57:37 +0430

    desgined further more sign up page

--- user/forms.py
+++ user/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-CSS_CLASS = 'form-control bg-dark text-white customize-input p-0 ps-2 mb-2'
+CSS_CLASSES = 'form-control bg-dark text-white customize-input p-0 ps-2 mb-2'
 LABELS = {'username': 'Username', 'password1': 'Password', 'password2': 'Confirm Password', 'email': 'Email'}
 
 class UserRegistrationForm(UserCreationForm):
@@ -12,7 +12,7 @@ class UserRegistrationForm(UserCreationForm):
 
         for field_name, label in LABELS.items():
             self.fields[field_name].label = label
-            self.fields[field_name].widget.attrs.update({'class': CSS_CLASS, 'placeholder': label, 'autofocus': False})
+            self.fields[field_name].widget.attrs.update({'class': CSS_CLASSES, 'placeholder': label, 'autofocus': False})
 
     class Meta:
         model = User
@@ -24,6 +24,6 @@ class CustomLoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
 
         for label, field in self.fields.items():
-            field.widget.attrs.update({'class': CSS_CLASS, 'placeholder': 'Enter ' + label.title()})
+            field.widget.attrs.update({'class': CSS_CLASSES, 'placeholder': 'Enter ' + label.title()})
 
     
